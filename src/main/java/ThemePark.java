7ac6e0e6a1af617b84ee131f66e8d85001c5e78f
helenosheaa <helenosheaa@gmail.com>
2018-07-02 16:22:24 +0100

    setup ThemePark

---
+++ src/main/java/ThemePark.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class ThemePark {
+
+    private String name;
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+
+    public ThemePark(String name) {
+        this.name = name;
+        attractions = new ArrayList<>();
+        stalls = new ArrayList<>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public ArrayList getAttractions(){
+        return new ArrayList(attractions);
+    }
+
+    public ArrayList getStalls(){
+        return new ArrayList(stalls);
+    }
+}
---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThemeParkTest {
+
+    ThemePark themePark;
+
+    @Before
+    public void before(){
+        themePark = new ThemePark("CodeClan Wondahs");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("CodeClan Wondahs", themePark.getName());
+    }
+
+
+}
