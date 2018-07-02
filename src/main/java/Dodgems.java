5030eab5dbb3663625bff369ba9f78c4c59ef217
helenosheaa <helenosheaa@gmail.com>
2018-07-02 16:02:49 +0100

    dodgems & rollercoaster extend to attraction

--- src/main/java/Dodgems.java
+++ src/main/java/Dodgems.java
@@ -1,2 +1,6 @@
-public class Dodgems {
+public class Dodgems extends Attraction {
+
+    public Dodgems(String name) {
+        super(name);
+    }
 }
--- src/test/java/DodgemsTest.java
+++ src/test/java/DodgemsTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DodgemsTest {
+
+    Dodgems dodgems;
+
+    @Before
+    public void before(){
+        dodgems = new Dodgems("Doi Dodgems");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Doi Dodgems", dodgems.getName());
+    }
 }
