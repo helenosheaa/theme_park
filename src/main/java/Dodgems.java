1c4cfb3f3cae662cf11a655567b800debff9cc49
helenosheaa <helenosheaa@gmail.com>
2018-07-02 16:57:00 +0100

    Dodgems implements ISecurity

--- src/main/java/Dodgems.java
+++ src/main/java/Dodgems.java
@@ -1,6 +1,11 @@
-public class Dodgems extends Attraction {
+public class Dodgems extends Attraction implements ISecurity {
 
     public Dodgems(String name) {
         super(name);
     }
+
+    public boolean isAllowedToVisit(Visitor visitor) {
+        return visitor.getAge() <= 14;
+    }
 }
+
--- src/test/java/DodgemsTest.java
+++ src/test/java/DodgemsTest.java
@@ -6,14 +6,21 @@ import static org.junit.Assert.assertEquals;
 public class DodgemsTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
 
     @Before
     public void before(){
         dodgems = new Dodgems("Doi Dodgems");
+        visitor1 = new Visitor(13,140,5.00);
     }
 
     @Test
     public void hasName(){
         assertEquals("Doi Dodgems", dodgems.getName());
+    }
+
+    @Test
+    public void canVisitAttraction(){
+        assertEquals(true, dodgems.isAllowedToVisit(visitor1));
     }
 }
