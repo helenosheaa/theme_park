185b779fd42491ccc6e0cebdf16af727e0974078
helenosheaa <helenosheaa@gmail.com>
2018-07-02 15:56:11 +0100

    Stall abstract class setup

--- src/main/java/Stall.java
+++ src/main/java/Stall.java
@@ -1,2 +1,24 @@
-public class Stall {
+public abstract class Stall {
+
+    private String name;
+    private String ownerName;
+    private String parkingSpot;
+
+    public Stall(String name, String ownerName, String parkingSpot){
+        this.name = name;
+        this.ownerName = ownerName;
+        this.parkingSpot = parkingSpot;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getOwnerName(){
+        return this.ownerName;
+    }
+
+    public String getParkingSpot(){
+        return this.parkingSpot;
+    }
 }
