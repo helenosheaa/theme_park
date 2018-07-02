ff1e41cea2225e385d560e9d3fb49977e9d2e16b
helenosheaa <helenosheaa@gmail.com>
2018-07-02 15:53:59 +0100

    Attraction abstract class has name

--- src/main/java/Attraction.java
+++ src/main/java/Attraction.java
@@ -1,2 +1,13 @@
-public class Attraction {
+public abstract class Attraction {
+
+    private String name;
+
+    public Attraction(String name){
+        this.name = name;
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
